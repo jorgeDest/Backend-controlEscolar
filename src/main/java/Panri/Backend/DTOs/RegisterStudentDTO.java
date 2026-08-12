9c21410a912662c773e20ab7f663d6117ab50ca4
jorgeDest <jg.paniagua21@info.uas.edu.mx>
2026-08-11 23:17:48 -0700

    logintud matricula

--- src/main/java/Panri/Backend/DTOs/RegisterStudentDTO.java
+++ src/main/java/Panri/Backend/DTOs/RegisterStudentDTO.java
@@ -15,6 +15,7 @@ public class RegisterStudentDTO {
     @NotBlank(message = "El apellido es obligatorio")
     private String lastName;
     @NotBlank(message = "La matricula del alumno es obligatoria")
+    @Size(min = 6 , message = "La matricula del alumno debe de ser ")//defino aqui o defino mediante un algoritmo que genere 6 cifras pero que no se repita la matricula
     private String enrollmentCode;
     @NotBlank(message = "La contraseña es obligatoria")
     @Size(min = 8, max = 12, message = "la contraseña debe tener entre 8 y 12 caracteres")
